2a8d4a4e36bede1afcc7a49d24c6e92629809a31
Julio Nava <julio.nava@caffenio.com>
2024-09-04 04:46:10 -0700

    [ADD] --> Siguientes metodos de la lista

--- src/listaenlazada/ListaEnlazada.java
+++ src/listaenlazada/ListaEnlazada.java
@@ -13,6 +13,7 @@ public class ListaEnlazada {
     private Nodo inicio;
     private Nodo fin;
     
+    
     public ListaEnlazada() {
         this.inicio = null;
         this.fin = null;
@@ -23,16 +24,17 @@ public class ListaEnlazada {
     }
     
     public void agregarAlInicio(String val) {
+        
         if(estaVacia()){
             inicio = new Nodo(val, inicio);
             fin = inicio;
         }else{
             inicio = new Nodo(val, inicio);
-            
         }
     }
     
     public void agregarAlFinal(String val) {
+        
         if(estaVacia()){
             inicio = new Nodo(val);
             fin = inicio;
@@ -53,8 +55,8 @@ public class ListaEnlazada {
     public void mostrarLista() {
         Nodo aux = this.inicio;
         while(aux != null){
-            System.out.print("["+ aux.getValor() +"]-->");
-            aux = aux.getSiguiente();
+            System.out.print("["+ aux.valor +"]-->");
+            aux = aux.siguiente;
         }
     }
     
@@ -64,12 +66,51 @@ public class ListaEnlazada {
         int contador = 0;
         while(aux != null){
             contador++;
-            aux = aux.getSiguiente();
+            aux = aux.siguiente;
         }
         return contador;
     }
     
+    public boolean buscarDato(String d) {
+        boolean encontrado = false;
+        Nodo aux = inicio;
+        while(encontrado != true && aux != null) {
+            if(d.equals(aux.valor)) {
+                encontrado = true;
+                break;
+            }else{
+                aux = aux.siguiente;
+            }
+        }
+        
+        return encontrado;
+    }
     
+    public String eliminarDatoInicio() {
+        String eliminado = inicio.valor;
+        int contador = cantidadNodos();
+        if(contador == 1){
+            inicio = null;
+            fin = inicio;
+        }else{
+            inicio = inicio.siguiente; //inicio.siguiente Accedemos al siguiente del nodo del cual apunta inicio.
+        }
+        
+        return eliminado;
+    }
+    
+    public String eliminarDatoFin() {
+        String eliminado = fin.valor;
+        Nodo aux = inicio;
+        while(aux.siguiente != fin) {
+            aux = aux.siguiente;
+        }
+        
+        fin = aux;
+        fin.siguiente = null;
+        
+        return eliminado;
+    }
     
 
     /**
--- src/listaenlazada/Main.java
+++ src/listaenlazada/Main.java
@@ -4,6 +4,9 @@
  */
 package listaenlazada;
 
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author julio.nava
@@ -15,9 +18,9 @@ public class Main {
      */
     public static void main(String[] args) {
         
-        /*
+        
         //PRIMERA PARTE
-        ListaEnlazada listaEnlazada = new ListaEnlazada();
+        /*ListaEnlazada listaEnlazada = new ListaEnlazada();
         
         listaEnlazada.agregarAlInicio("Juan");
         listaEnlazada.agregarAlInicio("Ana");
@@ -60,6 +63,144 @@ public class Main {
         System.out.println("Primer elemento de la lista: " + listaEnlazada.primerElemento());
         System.out.println("Ultimo elemento de la lista: " + listaEnlazada.ultimoElemento());*/
         
+        //----------------------------------------  04/09/2024
+        /*
+        //TERCERA PARTE
+        ListaEnlazada listaEnlazada = new ListaEnlazada();
+        listaEnlazada.agregarAlFinal("Juan");
+        listaEnlazada.agregarAlFinal("Ana");
+        listaEnlazada.agregarAlFinal("Lupita");
+        listaEnlazada.agregarAlFinal("Maria");
+        listaEnlazada.mostrarLista();
+        System.out.println("");
+        System.out.println("------------------------------------");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el nombre a buscar:");
+        String buscar = sc.next();
+        if(listaEnlazada.buscarDato(buscar)){
+            System.out.println("El dato se encuentra.");
+        }else{
+            System.out.println("El dato no se encuentra.");
+        }
+        */
+        
+        /*
+        //CUARTA PARTE
+        ListaEnlazada listaEnlazada = new ListaEnlazada();
+        listaEnlazada.agregarAlFinal("Juan");
+        listaEnlazada.agregarAlFinal("Ana");
+        listaEnlazada.agregarAlFinal("Lupita");
+        listaEnlazada.agregarAlFinal("Maria");
+        listaEnlazada.mostrarLista();
+        System.out.println("");
+        System.out.println("------------------------------------");
+        System.out.println("Cantidad de elementos en la lista: " + listaEnlazada.cantidadNodos());
+        System.out.println("Eliminando el primer elemento:");
+        System.out.println(listaEnlazada.eliminarDatoInicio());
+        System.out.println("------------------------------------");
+        listaEnlazada.mostrarLista();
+        System.out.println("");
+        */
+        
+        /*
+        //QUINTA PARTE
+        ListaEnlazada listaEnlazada = new ListaEnlazada();
+        listaEnlazada.agregarAlFinal("Juan");
+        listaEnlazada.agregarAlFinal("Ana");
+        listaEnlazada.agregarAlFinal("Lupita");
+        listaEnlazada.agregarAlFinal("Maria");
+        listaEnlazada.mostrarLista();
+        System.out.println("");
+        System.out.println("------------------------------------");
+        System.out.println("Cantidad de elementos en la lista: " + listaEnlazada.cantidadNodos());
+        System.out.println("Eliminando el ultimo elemento:");
+        System.out.println(listaEnlazada.eliminarDatoFin());
+        System.out.println("------------------------------------");
+        listaEnlazada.mostrarLista();
+        System.out.println("");
+        System.out.println("------------------------------------");
+        System.out.println("Cantidad de elementos en la lista: " + listaEnlazada.cantidadNodos());
+        */
+        
+        /*
+        //SEXTA PARTE
+        mostrarMenu();
+        */
+        
+    }
+    
+    public static void mostrarMenu(){
+        int opcion = 0;
+        ListaEnlazada lista = new ListaEnlazada();
+        
+        do {
+            try {
+            
+                opcion = Integer.parseInt( JOptionPane.showInputDialog(null, ""
+                                + "1.- Agregar al inicio\n2.- Agregar al final\n"
+                                + "3.- Buscar un dato\n4.- Saber la cantidad de nodos\n"
+                                + "5.- Primer elemento de la lista\n"
+                                + "6.- Último elemento de la lista\n"
+                                + "7.- Eliminar del inicio de la lista\n"
+                                + "8.- Eliminar al final de la lista\n"
+                                + "9.- Saber si la lista está vacía\n"
+                                + "10.- Mostrar la lista\n"
+                                + "11.- Salir\n"
+                                + "Ingrese una opción:\n") );
+                
+                switch (opcion) {
+                    case 1:
+                        String datoInicio = JOptionPane.showInputDialog(null, "Ingrese el dato:");
+                        lista.agregarAlInicio(datoInicio);
+                        break;
+                    case 2:
+                        String datoFinal = JOptionPane.showInputDialog(null, "Ingrese el dato:");
+                        lista.agregarAlFinal(datoFinal);
+                        break;
+                    case 3:
+                        String datoBuscar = JOptionPane.showInputDialog(null, "Ingrese el dato:");
+                        if(lista.buscarDato(datoBuscar)) {
+                            JOptionPane.showMessageDialog(null, "Dato encontrado en la lista");
+                        }else{
+                            JOptionPane.showMessageDialog(null, "Dato NO encontrado en la lista");
+                        }
+                        break;
+                    case 4:
+                        JOptionPane.showMessageDialog(null, "Cantidad de nodos en la lista: " + lista.cantidadNodos());
+                        break;
+                    case 5:
+                        JOptionPane.showMessageDialog(null, "El primer elemento de la lista es: " + lista.primerElemento());
+                        break;
+                    case 6:
+                        JOptionPane.showMessageDialog(null, "El último elemento de la lista es: " + lista.ultimoElemento());
+                        break;
+                    case 7:
+                        JOptionPane.showMessageDialog(null, "Dato eliminado del inicio: " + lista.eliminarDatoInicio());
+                        break;
+                    case 8:
+                        JOptionPane.showMessageDialog(null, "Dato eliminado del fin: " + lista.eliminarDatoFin());
+                        break;
+                    case 9:
+                        if(lista.estaVacia()){
+                            JOptionPane.showMessageDialog(null, "La lista ESTA vacia");
+                        }else{
+                            JOptionPane.showMessageDialog(null, "La lista NO ESTA vacia");
+                        }
+                        break;
+                    case 10:
+                        lista.mostrarLista();
+                        System.out.println("");
+                        break;
+                    case 11:
+                        JOptionPane.showMessageDialog(null, "Programa finalizado.");
+                        break;
+                }
+                
+            }catch(Exception e){
+                System.out.println("Error. Ingrese un entero: " + e.getMessage());
+            }
+            
+        }while(opcion != 11);
     }
     
 }
